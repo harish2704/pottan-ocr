acf965e8d132e6ac1066a6168fc7f12450234f4d
Harish.K <harish2704@gmail.com>
2017-12-16 02:05:45 +0530

    Rm Obsolete function

--- extract-glyphs.py
+++ extract-glyphs.py
@@ -13,42 +13,6 @@ def main( fname, ourDir ):
     contours = gd.detectContours( rect=(2,2) )
     gd.extractContours( contours, ourDir )
 
-def extractFromRendered( fname, outDir ):
-    imageFileBaseName = path.basename( fname )
-    lableMap = readJson( './cache/glyph_labels.json' )
-    gd = GlyphExtractor( fname )
-    contours = gd.detectContours( rect=(1,1) )
-
-    os.makedirs( outDir, exist_ok=True )
-    rgb = gd.rgb
-    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
-    totalHeight = rgb.shape[0]
-    glyphLabels = readJson('./cache/glyph_labels.json')
-    totalGlyphCount = len( glyphLabels )
-    heightPerGlyph = totalHeight/totalGlyphCount
-
-    glyphCountrourMap = [ [] for i in range( totalGlyphCount ) ]
-    
-    for idx, ( x, y, w, h ) in enumerate(contours):
-        glyphId = int( y/heightPerGlyph )
-
-        r = float(cv2.countNonZero( small[y:y+h, x:x+w])) / (w * h)
-        #  if more than 90% area is filled, then it is a 'dash' symbol which is kept as placeholder for zwnj chihnas
-        label = glyphLabels[ glyphId ][1]
-        #  print ( r, label )
-        if( r > 0.3 ):
-            glyphCountrourMap[ glyphId ].append(( x, y, w, h,  label ))
-
-
-    for contList in glyphCountrourMap:
-        if( len( contList ) == 1 ):
-            ( x, y, w, h, label ) = contList[0]
-            if( w > 10 ):
-                croppedImg = rgb[y:y+h, x:x+w ]
-                cv2.imwrite( '%s/%s_%s.png'%( outDir, label, imageFileBaseName ), croppedImg )
-
-
 if __name__ == "__main__":
-    #  main( sys.argv[1],  sys.argv[2] )
-    extractFromRendered( sys.argv[1],  sys.argv[2] )
+    main( sys.argv[1],  sys.argv[2] )
 
