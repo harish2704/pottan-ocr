e1f799ff00abe6bf28991eeb69d12247767cd338
Harish.K <harish2704@gmail.com>
2018-01-01 02:52:43 +0530

    Rm unwanted code

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import json
 import cv2
 import numpy as np
-from scipy.misc import imresize
 from torch.autograd import Variable
 
 FINAL_W=32
@@ -70,44 +69,4 @@ class averager(object):
             res = self.sum / float(self.n_count)
         return res
 
-class resizeNormalize(object):
 
-    def __init__(self, size, interpolation='bilinear'):
-        self.size = size
-        self.interpolation = interpolation
-        self.toTensor = transforms.ToTensor()
-
-    def __call__(self, img):
-        img = imresize( img, self.size, interp=self.interpolation)
-        img = self.toTensor(img)
-        img.sub_(0.5).div_(0.5)
-        return img
-
-class alignCollate(object):
-
-    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
-        self.imgH = imgH
-        self.imgW = imgW
-        self.keep_ratio = keep_ratio
-        self.min_ratio = min_ratio
-
-    def __call__(self, batch):
-        images, labels = zip(*batch)
-        imgH = self.imgH
-        imgW = self.imgW
-        if self.keep_ratio:
-            ratios = []
-            for image in images:
-                _, h, w = image.shape
-                ratios.append(w / float(h))
-            ratios.sort()
-            max_ratio = ratios[-1]
-            imgW = int(np.floor(max_ratio * imgH))
-            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
-
-        import ipdb; ipdb.set_trace()
-        transform = resizeNormalize((imgW, imgH))
-        images = [transform(image) for image in images]
-        images = torch.cat([t.unsqueeze(0) for t in images], 0)
-
-        return images, labels
