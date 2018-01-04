8f4a0ef58ab7f7854dbcc6ab42bc82b3ccb296f3
Harish.K <harish2704@gmail.com>
2018-01-04 10:51:51 +0530

    * Update build script

--- misc/install-deps.sh
+++ misc/install-deps.sh
@@ -5,29 +5,24 @@ warp_ctc_target="$thisDir/warp_ctc_install"
 
 cd $thisDir
 
-wget -c https://github.com/baidu-research/warp-ctc/archive/master.zip -O warp-ctc.zip
+wget -c https://github.com/SeanNaren/warp-ctc/archive/pytorch_bindings.zip -O pytorch_bindings.zip
+unzip  pytorch_bindings.zip
+cd warp-ctc-pytorch_bindings
 
-unzip warp-ctc.zip
-
-cd warp-ctc-master
 mkdir build
 cd build
-
 cmake ../
 make install DESTDIR=$warp_ctc_target
 
-echo "export WARP_CTC_PATH=\"$warp_ctc_target/usr/lib\"" >> $thisDir/../env.sh
+echo "export WARP_CTC_PATH=\"$warp_ctc_target/usr/local/lib\"" >> $thisDir/../env.sh
 
 . $thisDir/../env.sh
-
-cd $thisDir
-
-wget -c https://github.com/SeanNaren/warp-ctc/archive/pytorch_bindings.zip -O pytorch_bindings.zip
-unzip  pytorch_bindings.zip   'warp-ctc-pytorch_bindings/pytorch_binding/*'
-cd warp-ctc-pytorch_bindings/pytorch_binding
+cd $thisDir/warp-ctc-pytorch_bindings/pytorch_binding
+export CUDA_HOME=$( readlink -f $(dirname $( which nvcc ) )/../ )
 
 python3 ./setup.py install --user
 
+# tar -czvf /output/war-ctc.tar.gz $warp_ctc_target; python3 ./setup.py bdist; cp dist/warpctc_pytorch-0.1.linux-x86_64.tar.gz /output/
 
 
 
