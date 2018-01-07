96c64368c0e6d1939166cfe641af0541f281c934
Harish.K <harish2704@gmail.com>
2018-01-08 00:54:45 +0530

    Cleanup wiki data collection script

--- collect-wiki-data.js
+++ collect-wiki-data.js
@@ -4,39 +4,76 @@
  * Copyright 2018 Harish.K<harish2704@gmail.com>
  */
 
-var bigXml = require('big-xml');
+var fs = require('fs');
+var Transform = require('stream').Transform;
 var infile = process.argv[2];
+
+var symbolsTobeIncluded = [
+' ',
+  '!',
+  '"',
+  '#',
+  '$',
+  '%',
+  '&',
+  "'",
+  '(',
+  ')',
+  '*',
+  '+',
+  ',',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+  '[',
+  '\\',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~',
+].join('');
+// Escape special chars used in regex
+symbolsTobeIncluded = symbolsTobeIncluded.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
 if( !infile || ( infile === '--help' ) ){
   console.log('Usage: node ./collect-wiki-data.js <wiki_dump.xml>');
   process.exit(-1);
 }
 
-var reader = bigXml.createReader( infile, /^(text)$/ );
+var preProcessPatterns = [
+'<[a-zA-Z\ \-_\"\']+>',
+'<\/[a-zA-Z]+>',
+'\s{2,}',
+'&[^\s]*;',
+'\\\>',
+'[\\[\\]\\(\\)\\{\\}\'"\-=\.\*_]{2,}'
+];
 
-var i =0;
-var preProcess1 = new RegExp( '[\\[\\]\(\)\{\}\'"\-=\.\*_]{2,}', 'gm' );
-var nonMl = new RegExp('[^\\[\\]\-_\?\;!\:\'\"\.\{\}0-9\u0d00-\u0d7f\u200C\u200D\ ]+', 'gm' );
-var out = [];
-reader.on('record', function(record) {
-  // console.log( i );
-  // if( i >= 0 ){
-  out.push( record.text );
-  // }
-  i++;
-});
+preProcessPatterns = new RegExp('('+ preProcessPatterns.join('|')+')', 'g');
+var matchingPattern = new RegExp('[\u0d00-\u0d7f\u200C\u200D][\u0d00-\u0d7f\u200C\u200D'+ symbolsTobeIncluded +']{2,}', 'g' );
+var parser = new Transform();
+parser._transform = function(data, encoding, done) {
+  data = data.toString();
+  data = data.replace( preProcessPatterns, '')
+  var match = data.toString().match( matchingPattern );
+  if( match ){
+    this.push( match.join('\n') );
+  }
+  done();
+};
 
-reader.on('end', function(){
-  out.forEach(function( str ){
-    if( str ){
-      str = str.replace( preProcess1, '' );
-      str = str.replace( nonMl, ' ' );
-      console.log( str );
-    }
-  });
-  console.error('Completed... ' + i + ' Pages' );
-});
-
-reader.on('error', function( err ){
-  console.error('Err', err );
-});
+reader = fs.createReadStream( infile )
+  .pipe( parser )
+  .pipe( process.stdout )
 
