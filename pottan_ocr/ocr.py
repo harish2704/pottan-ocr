49b4c74a931f6a1ac10f1749ad8b798c6d9a4172
Harish.K <harish2704@gmail.com>
2018-01-19 03:29:02 +0530

    * Fix ocr

--- pottan_ocr/ocr.py
+++ pottan_ocr/ocr.py
@@ -23,29 +23,31 @@ def loadImg( fname ):
     img = img.resize( (targetW, targetH ), Image.BILINEAR )
     return normaizeImg( np.array( img ) )
 
-def evalModel( model, img_path, current, total ):
+def evalModel( img_path, current, total ):
+    global crnnModel
     print( 'Progress %d/%d' %( current, total ), file=sys.stderr )
     image = loadImg( img_path )
     image = image.view(1, *image.size())
     image = Variable(image)
-    preds = model(image)
+    preds = crnnModel(image)
     _, preds = preds.max(2)
     preds = preds.transpose(1, 0).contiguous().view(-1)
     preds_size = Variable(torch.IntTensor([preds.size(0)]))
     return converter.decode(preds.data, preds_size.data, raw=False)
 
+def threadInitializer( opt ):
+    global crnnModel
+    crnnModel = crnn.CRNN(32, 1, converter.totalGlyphs, opt.nh )
+    utils.loadTrainedModel( crnnModel, opt )
+    crnnModel.eval()
+
 def main( opt ):
-    model = crnn.CRNN(32, 1, converter.totalGlyphs, 256)
-    utils.loadTrainedModel( model, opt )
-    model.eval()
     totalImages = len( opt.image_paths )
-    pool = multiprocessing.Pool( multiprocessing.cpu_count() )
-    results = [ pool.apply_async( evalModel, ( model, img_path, idx, totalImages ) ) for idx, img_path in enumerate( opt.image_paths ) ]
-    #  results = [ evalModel( model, img_path, idx, totalImages ) for idx, img_path in enumerate( opt.image_paths ) ]
+    pool = multiprocessing.Pool( multiprocessing.cpu_count(), initializer=threadInitializer, initargs=( opt, ) )
+    results = [ pool.apply_async( evalModel, ( img_path, idx, totalImages ) ) for idx, img_path in enumerate( opt.image_paths ) ]
     for idx, result in enumerate( results ):
         img_path = opt.image_paths[ idx ]
         text = result.get()[0]
-        #  text = result[0]
         if( opt.stdout ):
             print('%s:::%s' %( img_path, [ text ] ))
         else:
@@ -58,6 +60,7 @@ if( __name__ == '__main__' ):
     parser.add_argument('image_paths', help='Path image file', nargs="+" )
     parser.add_argument('--crnn', required=True, help="path to pre trained model state ( .pth file)")
     parser.add_argument('--cuda', action='store_true', help='enables cuda')
+    parser.add_argument('--nh', type=int, default=256, help='size of the lstm hidden state')
     parser.add_argument('--stdout', action='store_true', help='Write output to stdout instead of saving it as <imgfile>.txt')
     opt = parser.parse_args()
     main( opt )
