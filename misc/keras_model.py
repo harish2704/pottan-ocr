2b2e471c5182e0348ba88e4fbd1b8eecf5477f62
Harish.K <harish2704@gmail.com>
2018-01-24 03:29:20 +0530

    Keras model

---
+++ misc/keras_model.py
@@ -0,0 +1,59 @@
+
+import keras
+from keras import backend as K
+from keras.models import Sequential, Model
+from keras.engine.topology import Layer
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, MaxPooling2D, TimeDistributed, Input, Embedding, Bidirectional, LSTM
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Permute, Lambda, Reshape
+K.set_image_data_format('channels_first')
+
+
+def KerasCrnn(imgH=32, nc=1, nclass=134, nh=64 ):
+
+    ks = [3, 3, 3, 3, 3, 3, 2]
+    ps = [1, 1, 1, 1, 1, 1, 0]
+    ss = [1, 1, 1, 1, 1, 1, 1]
+    nm = [64, 128, 256, 256, 512, 512, 512]
+
+    cnn = Sequential()
+
+    def convRelu(i, batchNormalization=False):
+        nIn = nc if i == 0 else nm[i - 1]
+        nOut = nm[i]
+
+        if( i == 0):
+            cnn.add( ZeroPadding2D(padding=1, input_shape=( 1, 32, 32 ) ) )
+            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], padding='valid', name='conv{0}'.format(i) ) )
+        else:
+            if( ps[i] != 0 ):
+                cnn.add( ZeroPadding2D(padding=ps[i] ) )
+            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], padding='valid', name='conv{0}'.format(i) ) )
+        if batchNormalization:
+            cnn.add( BatchNormalization(axis=1, momentum=0.1, epsilon=1e-5, name='batchnorm{0}'.format(i) ))
+        cnn.add( Activation('relu', name='relu{0}'.format(i) ))
+
+    convRelu(0)
+    cnn.add( MaxPooling2D( pool_size=2, strides=2, name='pooling{0}'.format(0) ) )  # 64x16x64
+    convRelu(1)
+    cnn.add( MaxPooling2D( pool_size=2, strides=2, name='pooling{0}'.format(1) ) )  # 128x8x32
+    convRelu(2, True)
+    convRelu(3)
+    cnn.add( ZeroPadding2D( padding=(0,1) ))
+    cnn.add( MaxPooling2D( pool_size=(2, 2), strides=(2, 1), padding='valid', name='pooling{0}'.format(2) ) )  # 256x4x16
+    convRelu(4, True)
+    convRelu(5)
+    cnn.add( ZeroPadding2D( padding=(0,1) ))
+    cnn.add( MaxPooling2D( pool_size=(2, 2), strides=(2, 1), padding='valid', name='pooling{0}'.format(3) ) )  # 512x2x16
+    convRelu(6, True)  # 512x1x16
+
+    cnn.add(Reshape((512, -1)))
+    cnn.add(Permute((2, 1)))
+    cnn.add(Bidirectional( LSTM(64 , return_sequences=True, use_bias=True, recurrent_activation='sigmoid', )) )
+    cnn.add( TimeDistributed( Dense(64) ) )
+    cnn.add(Bidirectional( LSTM(64 , return_sequences=True, use_bias=True, recurrent_activation='sigmoid', )) )
+    cnn.add( TimeDistributed(Dense( nclass ) ) )
+
+    cnn.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta())
+    return cnn
+
+
---
+++ tools/torch_to_keras.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+from collections import namedtuple
+import torch
+import torch.nn as nn
+from torch.autograd import Variable as Variable
+
+import keras
+from keras import backend as K
+from keras.models import Sequential, Model
+from keras.engine.topology import Layer
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, MaxPooling2D, TimeDistributed, Input, Embedding, Bidirectional, LSTM
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Permute, Lambda, Reshape
+
+from pottan_ocr.model import CRNN as CRNNT
+from misc.keras_model import KerasCrnn as CRNNK
+from pottan_ocr import string_converter as converter
+from pottan_ocr import utils
+
+dictToObj = lambda x: namedtuple('Struct', x.keys() )( *x.values() )
+opt = dictToObj({
+    'crnn': '/home/hari/Downloads/netCRNN_01-19-06-09-54_3.pth',
+    'cuda': False,
+    'nh': 64
+    })
+
+
+Tcrnn = CRNNT(32, 1, converter.totalGlyphs, opt.nh )
+utils.loadTrainedModel( Tcrnn, opt )
+Tcrnn.eval()
+
+Kcrnn = CRNNK( imgH=32, nc=1, nclass=converter.totalGlyphs, nh=opt.nh )
+
+layerMapping = {
+        'cnn.batchnorm2': 'batchnorm2',
+        'cnn.batchnorm4': 'batchnorm4',
+        'cnn.batchnorm6': 'batchnorm6',
+        'cnn.conv0': 'conv0',
+        'cnn.conv1': 'conv1',
+        'cnn.conv2': 'conv2',
+        'cnn.conv3': 'conv3',
+        'cnn.conv4': 'conv4',
+        'cnn.conv5': 'conv5',
+        'cnn.conv6': 'conv6',
+        'rnn.0.embedding': 'time_distributed_1',
+        'rnn.0.rnn' : 'bidirectional_1',
+        'rnn.1.embedding' : 'time_distributed_2',
+        'rnn.1.rnn': 'bidirectional_2'
+        }
+
+torchLayerMap = dict( Tcrnn.named_modules() )
+
+def layerNameFromParamKey( paramKey ):
+    return paramKey[:paramKey.rindex('.')]
+
+
+def LSTM( tLayer, kLayer ):
+    if( tLayer.bidirectional != True ):
+        raise NotImplemented('Sorry')
+
+    stateDict = dictToObj( tLayer.state_dict() )
+    newKparams = [
+           stateDict.weight_ih_l0.transpose(1,0),
+           stateDict.weight_hh_l0.transpose(1,0),
+           stateDict.bias_ih_l0                     + stateDict.bias_hh_l0,
+           stateDict.weight_ih_l0_reverse.transpose(1,0),
+           stateDict.weight_hh_l0_reverse.transpose(1,0),
+           stateDict.bias_ih_l0_reverse             + stateDict.bias_hh_l0_reverse,
+           ]
+    kLayer.set_weights( newKparams )
+
+def Linear( tLayer, kLayer ):
+    stateDict = dictToObj( tLayer.state_dict() )
+    newKparams = [
+            stateDict.weight.transpose(1,0).numpy(),
+            stateDict.bias.numpy(),
+            ]
+    kLayer.set_weights( newKparams )
+
+def BatchNorm2d( tLayer, kLayer ):
+    newKparams = [ i.numpy() for i in tLayer.state_dict().values() ]
+    kLayer.set_weights( newKparams )
+
+def Conv2d( tLayer, kLayer ):
+    newKparams = [ i.numpy() for i in tLayer.state_dict().values() ]
+    newKparams[0] = newKparams[0].transpose( 2,3,1,0 )
+    kLayer.set_weights( newKparams )
+
+transferFunctions = {
+        'Conv2d': Conv2d,
+        'LSTM': LSTM,
+        'Linear': Linear,
+        'BatchNorm2d': BatchNorm2d
+        }
+
+torchStateDict = Tcrnn.state_dict()
+for torchLayerName in set( [ layerNameFromParamKey(i) for i in torchStateDict ]):
+    torchLayer = torchLayerMap[ torchLayerName ]
+    kerasLayerName = layerMapping[ torchLayerName ]
+    try:
+        kerasLayer = Kcrnn.get_layer( name=kerasLayerName )
+    except Exception as e:
+        print( e )
+        continue
+    print( 'Transfering %s ---> %s' %( torchLayerName, kerasLayerName ) )
+
+    torchLayerType = type( torchLayer ).__name__
+    transferFunctions[ torchLayerType ]( torchLayer, kerasLayer )
+
+
+
+from pottan_ocr.ocr import loadImg
+Tip = loadImg('/home/hari/tmp/ocr-related/keras-js/demos/data/test.jpg').unsqueeze(0)
+
+
+#  TcnnBig = nn.Sequential( *list( list( Tcrnn.children() )[0].children() )[:] )
+#  TcnnBig.eval()
+#  KcnnBig = Sequential( Kcrnn.layers[:] )
+TcnnBig = Tcrnn
+KcnnBig = Kcrnn
+ToutBig = TcnnBig( Variable(Tip) )
+KoutBig = torch.from_numpy( KcnnBig.predict( Tip.numpy() ) )
+
+#  TcnnA = nn.Sequential( *list( list( Tcrnn.children() )[0].children() )[:7] )
+#  KcnnA = Sequential( Kcrnn.layers[:10] )
+#  ToutA = TcnnA( Variable(Tip) ).data.numpy()
+#  KoutA = KcnnA.predict( Tip.numpy() )
+
+#  TcnnB = nn.Sequential( *list( list( Tcrnn.children() )[0].children() )[7:8] )
+#  def genModel( lay ):
+    #  ip = Input( shape=lay.input_shape[1:] )
+    #  op = lay( ip )
+    #  model = Model( inputs=ip, outputs=op )
+    #  return model
+#  KcnnB = genModel( Kcrnn.layers[10] )
+#  ToutB = TcnnB( Variable( torch.from_numpy( ToutA ) ) ).data.numpy()
+#  KoutB = KcnnB.predict( KoutA )
+
+diff = KoutBig - ToutBig.transpose( 1,0,2 )
+print( 'Max: ', diff.max() )
