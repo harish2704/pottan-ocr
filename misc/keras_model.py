9c31ac4aeb7bfd9a5dfae479acf19b8c97bcd3bb
Harish.K <harish2704@gmail.com>
2018-01-24 18:57:00 +0530

    * Cleanup unwanted padding layer. Integrate it into conv2d layer

--- misc/keras_model.py
+++ misc/keras_model.py
@@ -5,7 +5,7 @@ from keras.models import Sequential, Model
 from keras.engine.topology import Layer
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, MaxPooling2D, TimeDistributed, Input, Embedding, Bidirectional, LSTM
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Permute, Lambda, Reshape
-K.set_image_data_format('channels_first')
+K.set_image_data_format('channels_last')
 
 
 def KerasCrnn(imgH=32, nc=1, nclass=134, nh=64 ):
@@ -17,19 +17,17 @@ def KerasCrnn(imgH=32, nc=1, nclass=134, nh=64 ):
 
     cnn = Sequential()
 
-    def convRelu(i, batchNormalization=False):
+    def convRelu(i, batchNormalization=False ):
         nIn = nc if i == 0 else nm[i - 1]
         nOut = nm[i]
+        padding = 'same' if ps[i] else 'valid'
 
         if( i == 0):
-            cnn.add( ZeroPadding2D(padding=1, input_shape=( 1, 32, 32 ) ) )
-            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], padding='valid', name='conv{0}'.format(i) ) )
+            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], input_shape=( 32, 640, 1 ), padding=padding, name='conv{0}'.format(i) ) )
         else:
-            if( ps[i] != 0 ):
-                cnn.add( ZeroPadding2D(padding=ps[i] ) )
-            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], padding='valid', name='conv{0}'.format(i) ) )
+            cnn.add( Conv2D( nOut, ks[i], strides=ss[i], padding=padding, name='conv{0}'.format(i) ) )
         if batchNormalization:
-            cnn.add( BatchNormalization(axis=1, momentum=0.1, epsilon=1e-5, name='batchnorm{0}'.format(i) ))
+            cnn.add( BatchNormalization(axis=3, momentum=0.1, epsilon=1e-5, name='batchnorm{0}'.format(i) ))
         cnn.add( Activation('relu', name='relu{0}'.format(i) ))
 
     convRelu(0)
@@ -44,10 +42,10 @@ def KerasCrnn(imgH=32, nc=1, nclass=134, nh=64 ):
     convRelu(5)
     cnn.add( ZeroPadding2D( padding=(0,1) ))
     cnn.add( MaxPooling2D( pool_size=(2, 2), strides=(2, 1), padding='valid', name='pooling{0}'.format(3) ) )  # 512x2x16
-    convRelu(6, True)  # 512x1x16
+    convRelu(6, True )  # 512x1x16
 
-    cnn.add(Reshape((512, -1)))
-    cnn.add(Permute((2, 1)))
+    cnn.add(Reshape((-1, 512)))
+    #  cnn.add(Permute((2, 1)))
     cnn.add(Bidirectional( LSTM(64 , return_sequences=True, use_bias=True, recurrent_activation='sigmoid', )) )
     cnn.add( TimeDistributed( Dense(64) ) )
     cnn.add(Bidirectional( LSTM(64 , return_sequences=True, use_bias=True, recurrent_activation='sigmoid', )) )
